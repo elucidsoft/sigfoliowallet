a66bc52f4d59f9d5e42372b2c34299e565fe08bc
Jai <devkirbyrawr@gmail.com>
2018-10-12 17:31:20 +0200

    Added converter IDToShortID for Stellar Address. Added SDKExtensions with the ShortAddress method. Added Converter to IssuerAddress of DataTemplate. Removed click selection on cards.

---
+++ SigfolioWallet.UWP/Converters/IDToShortID.cs
@@ -0,0 +1,33 @@
+﻿using SigfolioWallet.Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace SigfolioWallet.Converters
+{
+    public class IDToShortID : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string id = (string)value;
+            string shortedID = StellarSDKExtensions.ShortAddress(id);
+            if(id.Length == 56)
+            {
+                return shortedID;
+            }
+            else
+            {
+                return id;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
