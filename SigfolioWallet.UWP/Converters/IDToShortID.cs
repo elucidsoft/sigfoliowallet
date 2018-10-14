2376af8b76b702e70f63f388a131584035fc2090
Jai <devkirbyrawr@gmail.com>
2018-10-14 17:11:03 +0200

    Added parameter to IDToShortID converter for specify StartLength and EndLength Modified DataTemplate AssetCard for add 8 as start and end of length in issuer address.

--- SigfolioWallet.UWP/Converters/IDToShortID.cs
+++ SigfolioWallet.UWP/Converters/IDToShortID.cs
@@ -13,8 +13,20 @@ namespace SigfolioWallet.Converters
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
+            //Get Parameter StartLength|EndLength
+            int charactersStart = 5;
+            int charactersEnd = 4;
+
+            string parameterString = parameter as string;
+            if (!string.IsNullOrEmpty(parameterString))
+            {
+                string[] parameters = parameterString.Split(new char[] { '|' });
+                charactersStart = int.Parse(parameters[0]);
+                charactersEnd = int.Parse(parameters[1]);
+            }
+
             string id = (string)value;
-            string shortedID = StellarSDKExtensions.ShortAddress(id);
+            string shortedID = StellarSDKExtensions.ShortAddress(id, charactersStart, charactersEnd);
             if(id.Length == 56)
             {
                 return shortedID;
