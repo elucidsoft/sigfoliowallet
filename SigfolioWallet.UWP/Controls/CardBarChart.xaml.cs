4ffb88332c7a361607fd9b1105787d74936b3339
Eric Malamisura <eric@elucidsoft.com>
2018-11-12 08:53:17 -0500

    Minor changes

--- SigfolioWallet.UWP/Controls/CardBarChart.xaml.cs
+++ SigfolioWallet.UWP/Controls/CardBarChart.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using SigfolioWallet.Utilities;
 using System;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.Drawing;
-using System.Globalization;
-using System.Linq;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -16,12 +12,14 @@ using Color = Windows.UI.Color;
 namespace SigfolioWallet.Controls
 {
 
-    public sealed partial class CardChart : UserControl
+    public sealed partial class CardBarChart : UserControl
     {
-        public CardChart()
+        public CardBarChart()
         {
             InitializeComponent();
             Loaded += Card_Loaded;
+
+
         }
 
         private void Card_Loaded(object sender, RoutedEventArgs e)
@@ -44,69 +42,62 @@ namespace SigfolioWallet.Controls
             }
 
             data = new ObservableCollection<Data>();
-            
+
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             Random rnd = new Random();
             timer.Tick += (o, o1) =>
             {
-                Add(rnd.Next(2, 100));
+                Add(rnd.Next(1, 200), rnd.Next(1, 200), rnd.Next(2, 200));
             };
 
             timer.Start();
-            for (int i = 0; i <= 60; i++)
+
+            for (int i = 0; i <= 30; i++)
             {
-                data.Add(new Data() { Value = 0 });
+                data.Add(new Data()
+                {
+                    BarCategory = DateTime.Now,
+                    BarValue1 = 1,
+                    BarValue2 = 1
+                });
             }
 
-            lineSeries.DataContext = data;
+            chart.DataContext = data;
 
-            sasSeries.Stroke = new SolidColorBrush(Color);
+            var colorLow = UIUtility.GetAccentColorLow();
+            var color = UIUtility.GetAccentColor();
+            sbLineBrush.Color = color;
 
-            var color1 = Color;
+            var color1 = colorLow;
             color1.A = 94;
 
-            var color2 = Color;
+            var color2 = colorLow;
             color2.A = 0;
 
             gs1Color.Color = color1;
             gs2Color.Color = color2;
-            
-            data.CollectionChanged += Data_CollectionChanged;
+
+            sbColor1.Color = color;
+            sbColor2.Color = colorLow;
+
         }
 
-        private void Data_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-            if (e.Action == NotifyCollectionChangedAction.Add)
-            {
-                var firstValue = data[0].Value.ToString(CultureInfo.InvariantCulture);
+        //public static readonly DependencyProperty BarsBrushProperty = DependencyProperty.Register("BarsBrush", typeof(SolidColorBrush), typeof(CardBarChart), new PropertyMetadata(null));
+        //public SolidColorBrush BarsBrush { get => new SolidColorBrush(Colors.Yellow); }
 
-                var newValue = data[e.NewStartingIndex - 1].Value;
+        //public double? LowValue { get; set; }
 
-                LowValue = data.Min(a => a.Value);
-                HighValue = data.Max(a => a.Value);
+        //public double HighValue { get; set; }
 
-                if (LowValue != ((Data)e.NewItems[0]).Value)
-                    LowTextBlock.Text = $"{LowValue}/{newValue}";
-
-                if (HighValue != ((Data)e.NewItems[0]).Value)
-                    HighTextBlock.Text = $"{firstValue}/{HighValue}";
-
-            }
-        }
-
-        public double? LowValue { get; set; }
-
-        public double HighValue { get; set; }
-
-        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(CardChart), new PropertyMetadata(null));
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(CardBarChart), new PropertyMetadata(null));
         public string Label
         {
             get => this.GetValue<string>(LabelProperty);
             set => this.SetValue(LabelProperty, value);
         }
 
-        public static readonly DependencyProperty ColorProperty = DependencyProperty.Register("Color", typeof(Color), typeof(CardChart), new PropertyMetadata(null));
+        public static readonly DependencyProperty ColorProperty = DependencyProperty.Register("Color", typeof(Color), typeof(CardBarChart), new PropertyMetadata(null));
         public Color Color
         {
             get => this.GetValue<Color>(ColorProperty);
@@ -115,17 +106,32 @@ namespace SigfolioWallet.Controls
 
         public ObservableCollection<Data> data { get; set; }
 
-        private void Add(double value)
+        private void Add(double value1, double value2, double lineValue)
         {
-            if (data.Count >= 60)
+            if (data.Count >= 30)
                 data.RemoveAt(0);
 
-            data.Add(new Data() { Value = value });
+            data.Add(new Data()
+            {
+                BarCategory = DateTime.Now,
+                BarValue1 = value1,
+                BarValue2 = value2,
+                LineValue = lineValue
+            });
         }
 
         public class Data
         {
-            public double Value { get; set; }
+            public string LineCategory { get; set; }
+
+            public DateTime BarCategory { get; set; }
+
+            public double LineValue { get; set; }
+
+            public double BarValue1 { get; set; }
+
+
+            public double BarValue2 { get; set; }
         }
     }
 
